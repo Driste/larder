0debd96a6522a7e1131b3204de2567f1d1a46931
Driste <manofginger@gmail.com>
2014-04-22 15:29:32 -0400

    Post
    -added post request
    -recall api when clicked on for better performance

--- caller.php
+++ caller.php
@@ -3,7 +3,8 @@
     $id = "521b1d6a";
     
     $keyword = "onion+soup";
-
+	
+	//http://api.yummly.com/v1/api/recipe/$recId?_app_id=521b1d6a&_app_key=356970b560a7a1f993e48a203a2221d7
     //header("Location: http://api.yummly.com/v1/api/recipes?_app_id=$id&_app_key=$key&q=$keyword");
 
     $url = "http://api.yummly.com/v1/api/recipes?_app_id=$id&_app_key=$key&allowedIngredient[]=garlic&allowedIngredient[]=butter&allowedIngredient[]=potatoes&allowedIngredient[]=cheese&allowedIngredient[]=salt&allowedIngredient[]=chicken";
@@ -12,11 +13,16 @@
     $result = json_decode($presult);
 
     foreach($result->matches as $trend){
-        echo "{$trend->recipeName}\n<br>";
-        foreach($trend->ingredients as $ing){
+		
+		
+        echo "<strong>$trend->recipeName</strong><br>";
+        /*foreach($trend->ingredients as $ing){
             echo "$ing, ";
-        }
-        echo "<br><br>";
+        }*/
+		echo '<br />';
+		echo '<img src="'.reset($trend->smallImageUrls).'"\>';
+		echo '<a href="http://api.yummly.com/v1/api/recipe/'.$trend->id.'?_app_id=521b1d6a&_app_key=356970b560a7a1f993e48a203a2221d7">Show Recipe</a>';
+        echo '<br><br>';
     }
 
 ?>
